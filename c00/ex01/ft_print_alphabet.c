7bf1ecd80d6fe85e45cd90c05717568e78397f2b
Celal Serdar Ergun <celalserdarergun@gmail.com>
2022-02-13 17:37:32 +0300

    ex01 yapildi

---
+++ c00/ex01/ft_print_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cergun <cergun@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/10 16:30:14 by cergun            #+#    #+#             */
+/*   Updated: 2022/02/11 12:38:32 by cergun           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		write(1, letter, 1);
+		letter++;
+	}
+}
