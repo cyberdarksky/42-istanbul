cd394a37667e7056b92adaf356ac136b36178649
Celal Serdar Ergun <celalserdarergun@gmail.com>
2022-02-13 17:43:38 +0300

    ex05 yapildi

---
+++ c00/ex05/ft_print_comb.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cergun <cergun@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/11 12:25:45 by cergun            #+#    #+#             */
+/*   Updated: 2022/02/11 13:03:31 by cergun           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_comb(void)
+{
+	char	first;
+	char	second;
+	char	third;
+
+	first = '0';
+	while (first <= '9')
+	{
+		second = first + 1;
+		while (second <= '9')
+		{
+			third = second + 1;
+			while (third <= '9')
+			{
+				write(1, &first, 1);
+				write(1, &second, 1);
+				write(1, &third, 1);
+				write(1, ", ", 2);
+				third++;
+			}
+			second++;
+		}
+		first++;
+	}
+}
