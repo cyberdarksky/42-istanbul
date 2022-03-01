9328ffa71eb08c2332f236a05a945f13babbad0c
Celal Serdar Ergun <celalserdarergun@gmail.com>
2022-03-01 19:55:50 +0300

    1 March backup

--- bitmedi/7/ex03/ft_strjoin.c
+++ bitmedi/7/ex03/ft_strjoin.c
@@ -1,9 +1,78 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cergun <cergun@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/01 16:14:11 by cergun            #+#    #+#             */
+/*   Updated: 2022/03/01 18:40:40 by cergun           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strcpy(char	*dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i])
+		i++;
+	j = 0;
+	while (src[j])
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
 
 char	*ft_strjoin(int size, char **strs, char *sep)
 {
-	int	i;
+	char	*arr;
+	int		i;
 
 	if (size == 0)
-		return (0);
+	{
+		arr = malloc(1);
+		return (arr);
+	}
+	arr = malloc(sizeof(strs));
+	i = 0;
+	while (i < size)
+	{
+		ft_strcpy(arr, strs[i]);
+		if (i != size - 1)
+			ft_strcpy(arr, sep);
+		i++;
+	}
+	i = 0;
+	while (arr[i])
+		i++;
+	arr[i] = '\0';
+	return (arr);
 }
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char *text[10];
+
+	text[0] = "Testando";
+	text[1] = "a";
+	text[2] = "funcao";
+	text[3] = "ft_strjoin";
+	text[4] = "com";
+	text[5] = "o";
+	text[6] = "separador";
+	text[7] = "\' - \'";
+	text[8] = "e tamanho";
+	text[9] = "10";
+
+	printf("%s\n", ft_strjoin(10, text, " - "));
+}
+*/
--- bitmedi/7/ex04/ft_convert_base.c
+++ bitmedi/7/ex04/ft_convert_base.c
@@ -1,36 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*   ft_convert_base.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cergun <cergun@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/01 16:13:29 by cergun            #+#    #+#             */
-/*   Updated: 2022/03/01 16:13:51 by cergun           ###   ########.fr       */
+/*   Created: 2022/03/01 18:01:09 by cergun            #+#    #+#             */
+/*   Updated: 2022/03/01 18:40:07 by cergun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 
-int	ft_ultimate_range(int **range, int min, int max)
-{
-	int	i;
-	int	size;
-
-	if (min >= max)
-	{
-		*range = NULL;
-		return (0);
-	}
-	size = max - min;
-	*range = (int *)malloc(size * sizeof(int));
-	if (*range == NULL)
-		return (-1);
-	i = 0;
-	while (i < size)
-	{
-		(*range)[i] = min + i;
-		i++;
-	}
-	return (i);
-}
